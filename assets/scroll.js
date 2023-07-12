2cf97ecc43d9812d3f30090353b553a3c4c7856b
Boliboe <iamjohnsomto@gmail.com>
2023-07-12 23:51:09 +0100

    srolljs folder created

---
+++ assets/scroll.js
@@ -0,0 +1,11 @@
+let scrollCount = () => {
+    let scrollProgress = document.getElementById('progress');
+    let progressValue = document.getElementById('progress-value');
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round(pos * 100 / calcHeight);
+    
+    console.log(scrollValue);
+}
+
+window.onscroll = scrollCount;
