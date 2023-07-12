8caa8f8949226bc97234f2354643942f8c20fa4d
Boliboe <iamjohnsomto@gmail.com>
2023-07-13 00:06:16 +0100

    scrollvalue added in js file

--- assets/scroll.js
+++ assets/scroll.js
@@ -2,10 +2,13 @@ let scrollCount = () => {
     let scrollProgress = document.getElementById('progress');
     let progressValue = document.getElementById('progress-value');
     let pos = document.documentElement.scrollTop;
-    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollValue = Math.round(pos * 100 / calcHeight);
-    
+    let calcHeight = document.documentElement.
+    scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round( pos * 100 / calcHeight);
+
     console.log(scrollValue);
+
+    scrollProgress
 }
 
 window.onscroll = scrollCount;
